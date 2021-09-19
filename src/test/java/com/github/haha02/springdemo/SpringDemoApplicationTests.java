e99110ce90b74217d5bb28bb9a530452f37e9ca9
haha02 <ohyakuka@gmail.com>
2021-09-19 19:14:34 +0800

    Initial commit

---
+++ src/test/java/com/github/haha02/springdemo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.haha02.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
